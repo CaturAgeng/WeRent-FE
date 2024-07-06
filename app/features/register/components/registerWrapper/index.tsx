e1f154281ea08654f225a660d24e1f602e95c5d5
CaturAgeng <142207164+CaturAgeng@users.noreply.github.com>
2024-07-06 20:11:22 +0700

    Revert "Merged register form to branch main"

--- app/features/register/components/registerWrapper/index.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm } from "../registerForm";
-
-export function RegisterWrapper() {
-    return (
-        <div className="h-[100svh] flex justify-center items-center">
-            <RegisterForm/>
-        </div>
-    )
-}
