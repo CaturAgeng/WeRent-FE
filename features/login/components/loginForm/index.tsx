a18552ba1da77accafde33c3ea7d98f5c276956d
yuliussmayoru <yuliussmayoru@gmail.com>
2024-07-12 10:02:11 +0700

    adding name to fields

--- features/login/components/loginForm/index.tsx
+++ features/login/components/loginForm/index.tsx
@@ -48,6 +48,7 @@ export function LoginForm() {
           email: email!.toString(),
           password: password!.toString()
       }
+
       console.log(payload)
 
       mutate(payload)
@@ -56,34 +57,36 @@ export function LoginForm() {
   return (
     <Card className="space-y-2 shadow-lg p-5 rouded-lg">
         <form onSubmit={loginHandler} data-cy="register-form">
-        <div className="flex justify-center">
-          <Image
-            src="/dummy/19.png"
-            width={200}
-            height={100}
-            alt="WeRent Logo"
-            className="object-center"
-          />
-        </div>
+            <div className="flex justify-center">
+            <Image
+                src="/dummy/19.png"
+                width={200}
+                height={100}
+                alt="WeRent Logo"
+                className="object-center"
+            />
+            </div>
+
+            <p className="text-center text-xl mb-2">Login Form</p>
+            <Input label="E-mail" name="email" data-cy="email-input"/>
+            <Input
+                label="Password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                rightNode={showPassword ? <EyeCloseIcon /> : <EyeOpenIcon />}
+                rightNodeClick={showPasswordHandler}
+                data-cy="password-input"
+            />
+            <Button
+                type="submit"
+                data-cy="login-button"
+                className="bg-custom-blue text-white hover:bg-white hover:text-custom-blue font-bold py-2 px-4 rounded border border-custom-blue">
+                Login
+            </Button>
+            <p className="text-center text-xs">
+                Not registered? Click <a href="/register">Register</a>
+            </p>
         </form>
-      <p className="text-center text-xl mb-2">Login Form</p>
-      <Input label="E-mail" data-cy="email-input"/>
-      <Input
-        label="Password"
-        type={showPassword ? "text" : "password"}
-        rightNode={showPassword ? <EyeCloseIcon /> : <EyeOpenIcon />}
-        rightNodeClick={showPasswordHandler}
-        data-cy="password-input"
-      />
-      <Button
-        type="submit"
-        data-cy="login-button"
-        className="bg-custom-blue text-white hover:bg-white hover:text-custom-blue font-bold py-2 px-4 rounded border border-custom-blue">
-        Login
-      </Button>
-      <p className="text-center text-xs">
-        Not registered? Click <a href="/register">Register</a>
-      </p>
     </Card>
   );
 }
