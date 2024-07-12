7ee96d868f184332b6ea7c8b3494ee56dd0d789f
yuliussmayoru <yuliussmayoru@gmail.com>
2024-07-12 09:29:13 +0700

    mock(api/login):create api mockup for login

--- features/login/api/login.ts
+++ features/login/api/login.ts
@@ -1,10 +1,10 @@
-import { axiosInstance } from "api/axiosClients";
+import { apiClient } from "api";
 import { AxiosError } from "axios";
 import { loginRequestProps } from "features/login";
 
 export async function loginRequest( payload : loginRequestProps ) {
     try {
-        const {data} = await axiosInstance.post("/user/login", payload)
+        const {data} = await apiClient.post("/user/login", payload)
         return data
     } catch(error) {
         if (error instanceof AxiosError) {
