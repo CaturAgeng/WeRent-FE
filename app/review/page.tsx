7dff80398d49a8f00270541040b94b9ab7eb25b2
CaturAgeng <caturageng023@gmail.com>
2024-07-16 16:42:56 +0700

    Modified code

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,7 +1,20 @@
-export default function Page() {
-    return (
-        <div className="flex justify-center items-center h-screen w-screen">
-            <h1 className="text-3xl font-extrabold">Review Page</h1>
-        </div>
-    );
-}
+// export default function Page() {
+//     return (
+//         <div className="flex justify-center items-center h-screen w-screen">
+//             <h1 className="text-3xl font-extrabold">Review Page</h1>
+//         </div>
+//     );
+// }
+
+import React from "react";
+import ViewMoreWrapper from "@/features/review/components/viewMore/viewMoreWrapper";
+
+const ReviewsPage: React.FC = () => {
+  return (
+    <div>
+      <ViewMoreWrapper />
+    </div>
+  );
+};
+
+export default ReviewsPage;
